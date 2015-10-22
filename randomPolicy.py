9a6b62af1cc9b3014383a361cafc00bc9f74cf4d
Chenyang Huang <hchenyang@hotmail.com>
2015-10-22 10:09:17 -0600

    1

---
+++ randomPolicy.py
@@ -0,0 +1,13 @@
+import blackjack
+from pylab import *
+
+numEpisodes = 2000
+
+returnSum = 0.0
+print "DDD"
+for episodeNum in range(numEpisodes):
+    G = 0
+    
+    print "Episode: ", episodeNum, "Return: ", G
+    returnSum = returnSum + G
+print "Average return: ", returnSum/numEpisodes
