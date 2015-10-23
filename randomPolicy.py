afd8997508f86a58fe2eb9d5a7e532721a1705a3
Chenyang Huang <hchenyang@hotmail.com>
2015-10-22 18:12:44 -0600

    finish

--- randomPolicy.py
+++ randomPolicy.py
@@ -2,12 +2,13 @@ import blackjack
 from pylab import *
 
 numEpisodes = 2000
-
 returnSum = 0.0
-print "DDD"
 for episodeNum in range(numEpisodes):
     G = 0
-    
+    G, s=blackjack.sample(0,0)
+    while s != -1:
+        action = randint(0,blackjack.numActions(s))
+        G, s=blackjack.sample(s,action)
     print "Episode: ", episodeNum, "Return: ", G
     returnSum = returnSum + G
 print "Average return: ", returnSum/numEpisodes
